0905ac9a1432594618ec4bf2b192b63d04bca9f2
Oleg Bolshakov <oleg.bolshakov@bk.ru>
2019-03-04 01:02:04 +0300

    v1

---
+++ src/components/ContractorData.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="contractor_wrapper">
+		<div class="text_field_wrapper">
+			<input type="text" class="text_field">
+			<p class="text_field_error">dddddddd</p>
+		</div>
+		<div class="text_field_wrapper">
+			<input type="text" class="text_field">
+			<p class="text_field_error">dddddddddd</p>
+		</div>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "Contractor"
+    }
+</script>
+
+<style lang="scss" scoped>
+	@import "../assets/styles/contractor";
+</style>
---
+++ src/components/ObjectData.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="info">
+    <p class="object"><span class="object_header">Альфа:</span> {{block}} блок</p>
+    <p class="object"><span class="object_header">Квартира:</span>  №{{appartament}}</p>
+    <p class="object"><span class="object_header">Строительные оси:</span>  {{constructionAxis}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ObjectData',
+    props: ['change'],
+    mounted(){
+      this.randomiser()
+    },
+    watch: {
+      change () {
+        this.randomiser()
+      }
+    },
+    data () {
+      return {
+        block: '',
+        appartament: '',
+        constructionAxis: ''
+      }
+    },
+    methods:{
+      randomiser(){
+        this.block = Math.round(Math.random() * 5 + .5)
+  
+        this.appartament = Math.round(Math.random() * 100 - .5)
+  
+        this.constructionAxis = charGenerator().toUpperCase() + charGenerator() + charGenerator() +
+          '-' + Math.round(Math.random() * 100 - .5)
+      }
+    }
+  }
+  
+  function charGenerator () {
+    return String.fromCharCode(Math.floor(Math.random() * (97 - 119)) + 119)
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/styles/object-data.scss";
+</style>
